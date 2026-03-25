e1879b4ca6802826acbb53a4c5ade4b7bdf910fe
Твоё имя < email  GitHub>
2026-03-25 14:12:12 +0200

    refactor: update comments in assets.js for clarity and consistency

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,4 +1,4 @@
-// Символ ** означает "любая подпапка"
+// Symbol ** points to "any folder"
 const allAssets = import.meta.glob(
   '../assets/images/**/*.{png,jpg,jpeg,svg,webp}',
   { eager: true }
@@ -6,7 +6,7 @@ const allAssets = import.meta.glob(
 
 export const assetMap = Object.fromEntries(
   Object.entries(allAssets).map(([path, module]) => {
-    // Получаем только имя файла: "folder/sub/icon.svg" -> "icon.svg"
+    // Getting only file name: "folder/sub/icon.svg" -> "icon.svg"
     const filename = path.split('/').pop();
     return [filename, module.default];
   })
